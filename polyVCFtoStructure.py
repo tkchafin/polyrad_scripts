34af8f1b60c5bc98aad5459059384b6fcd435c90
Tyler Chafin <tkchafin@uark.edu>
2020-10-13 19:16:23 -0500

    menu done

--- polyVCFtoStructure.py
+++ polyVCFtoStructure.py
@@ -10,79 +10,37 @@ def main():
 	f = open(params.out, 'w')
 	
 	with open(params.vcf, "r") as vcf:
+		last_header=None
 		for line in vcf:
 			line=line.strip()
-			#directly transfer header lines
+			#skip headers
 			if line[0] == "#":
-				if line[1] != "#":
-					f.write("##FORMAT=<ID=AD,Number=.,Type=Integer,Description=\"Allelic depths for the reference and alternate alleles in the order listed\">\n")
-				f.write(line)
-				f.write("\n")
-				
+				last_header=line.split("\t")
+				continue
 			else:
 				fields=line.split("\t")
-				ref=get_index(fields[3].split(","))
-				alt=get_index(fields[4].split(","))
-				#if biallelic filter on and site has >2 alleles, skip
-				if params.biallelic == True:
-					if (len(ref) + len(alt) > 2):
-						continue
-				if fields[8]=="GT:DP:CATG":
-					fields[8]="GT:DP:AD"
-				else:
-					print("Something wrong with VCF. Field 8 should be GT:DP:CATG")
-					sys.exit()
-				for idx, sample in enumerate(fields[9:]):
-					fixed=str(fix_sample(sample, ref, alt))+":"
-					fields[idx+9] = fixed
-					#print(sample, " -- ", fixed)
-				f.write("\t".join(fields))
-				f.write("\n")
+
 		vcf.close()
 	f.close()
 
-def fix_sample(sample, ref, alt):
-	fields=sample.split(":")
-	catg=fields[2].split(",")
-	ad=list()
-	for r in ref:
-		ad.append(catg[r])
-	for a in alt:
-		ad.append(catg[a])
-	fields[2]=",".join(ad)
-	return(":".join(fields))
-	
-def get_index(char):
-	ret=list()
-	for c in char:
-		if c.lower()=="c":
-			ret.append(0)
-		elif c.lower()=="a":
-			ret.append(1)
-		elif c.lower()=="t":
-			ret.append(2)
-		elif c.lower()=="g":
-			ret.append(3)
-		else:
-			print("Unrecognized character",char)
-			sys.exit()
-	return(ret)
 
 #Object to parse command-line arguments
 class parseArgs():
 	def __init__(self):
 		#Define options
 		try:
-			options, remainder = getopt.getopt(sys.argv[1:], 'hv:o:b', \
-			["help", "vcf=", "out=", "biallelic"])
+			options, remainder = getopt.getopt(sys.argv[1:], 'hv:o:p:x:', \
+			["help"])
 		except getopt.GetoptError as err:
 			print(err)
 			self.display_help("\nExiting because getopt returned non-zero exit status.")
 		#Default values for params
 		#Input params
 		self.vcf=None
-		self.out="polyrad.vcf"
-		self.biallelic=False
+		self.out="polyrad.str"
+		self.popmap=None
+		self.extracols=0
+		self.locnames=False
 
 
 		#First pass to see if help menu was called
@@ -98,18 +56,20 @@ class parseArgs():
 			#print(opt,arg)
 			if opt == "h" or opt == "help":
 				continue
-			elif opt=="vcf" or opt=="v":
+			elif opt=="v":
 				self.vcf=arg
-			elif opt=="out" or opt=="o":
+			elif opt=="o":
 				self.out=arg
-			elif opt=="biallelic" or opt=="b":
-				self.biallelic=True
+			elif opt=="p":
+				self.popmap=arg
+			elif opt=="x":
+				self.extracols=int(arg)
 			else:
 				assert False, "Unhandled option %r"%opt
 
 		#Check manditory options are set
 		if not self.vcf:
-			self.display_help("Need an ipyrad VCF file")
+			self.display_help("Need a VCF file")
 
 
 
@@ -117,14 +77,15 @@ class parseArgs():
 		if message is not None:
 			print()
 			print (message)
-		print ("\nipyrad2polyrad.py\n")
+		print ("\npolyVCFtoStructure.py\n")
 		print("Author: Tyler K Chafin, University of Arkansas")
 		print ("Contact: tkchafin@uark.edu")
-		print ("Description:Converts the ipyrad VCF to a format usable for polyRAD")
+		print ("Description: Converts polyRAD VCF to Structure (.str)")
 		print("""
-		-v,--vcf	: VCF input with ipyrad "CATG" field
-		-b,--biallelic	: [Boolean] Toggle to skip non-biallelic sites
-		-o,--out	: Output file name (default=polyrad.vcf)
+		-v	: VCF input file formatted by polyRAD
+		-p	: (Optional) popmap file with pop labels
+		-x	: (Optional) number of extra (blank) columns to add
+\		-o	: Output file name (default=polyrad.vcf)
 """)
 		print()
 		sys.exit()

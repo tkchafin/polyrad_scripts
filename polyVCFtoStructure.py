151827c1a30c896b1cefebcb96f82c72a7f1f017
Tyler Chafin <tkchafin@uark.edu>
2020-11-17 14:13:27 -0700

    numbers pops in structure

--- polyVCFtoStructure.py
+++ polyVCFtoStructure.py
@@ -78,7 +78,7 @@ def main():
 def numberPops(popmap):
 	popID=dict()
 	index=0
-	for pop in map(list(popmap.values())):
+	for pop in popmap.values():
 		if pop not in popID.keys():
 			popID[pop]=index
 			index+=1
